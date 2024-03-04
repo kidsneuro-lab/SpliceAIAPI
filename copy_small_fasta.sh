78745f784210f4a0b095d810154416f1925c5efa
h-joshi <hihi.joshi@gmail.com>
2024-03-04 08:58:01 +0000

    Remove compression of fasta files

--- copy_small_fasta.sh
+++ copy_small_fasta.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp -v tests/data/chr21_grch37.fa.gz hg_ref/Homo_sapiens_assembly19.fasta.gz
-cp -v tests/data/chr21_grch37.fa.gz.fai hg_ref/Homo_sapiens_assembly19.fasta.gz.fai
+cp -v tests/data/chr21_grch37.fa hg_ref/Homo_sapiens_assembly19.fasta.gz
+cp -v tests/data/chr21_grch37.fa.fai hg_ref/Homo_sapiens_assembly19.fasta.gz.fai
 
-cp -v tests/data/chr21_grch38.fa.gz hg_ref/Homo_sapiens_assembly38.fasta.gz
-cp -v tests/data/chr21_grch38.fa.gz.fai hg_ref/Homo_sapiens_assembly38.fasta.gz.fai
+cp -v tests/data/chr21_grch38.fa hg_ref/Homo_sapiens_assembly38.fasta.gz
+cp -v tests/data/chr21_grch38.fa.fai hg_ref/Homo_sapiens_assembly38.fasta.gz.fai
