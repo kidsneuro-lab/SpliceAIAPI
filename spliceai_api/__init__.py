4e82665a53d2062c6b0258972e134e4f8aea3923
h-joshi <hihi.joshi@gmail.com>
2024-08-20 05:10:40 +0000

    Raise an error if ENSEMBL_TIMEOUT is not set

--- spliceai_api/__init__.py
+++ spliceai_api/__init__.py
@@ -1,3 +1,6 @@
 import os
 
+# Check for ENSEMBL_TIMEOUT environment variable
 ENSEMBL_TIMEOUT = os.getenv("ENSEMBL_TIMEOUT")
+if ENSEMBL_TIMEOUT is None:
+    raise EnvironmentError("Environment variable 'ENSEMBL_TIMEOUT' is not declared. Please set this variable before running the application.")
